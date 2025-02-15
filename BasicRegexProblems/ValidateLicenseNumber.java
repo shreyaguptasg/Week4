3037768a388ba429c4b75e2c3ae7f1e811785733
shrey <shreya272002gupta@gmail.com>
2025-02-15 11:58:31 +0530

    [Shreya] Add. Validate License Number

---
+++ BasicRegexProblems/ValidateLicenseNumber.java
@@ -0,0 +1,22 @@
+package Day5.BasicRegexProblems;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateLicenseNumber {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("[A-Z]{2}[0-9]{4}");
+        Scanner cs = new Scanner(System.in);
+        System.out.println("Enter a username");
+        String input = cs.next();
+        Matcher m = p.matcher(input);
+        boolean matches = m.matches();
+        if(matches){
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Invalid, Try again!user");
+        }
+    }
+}
